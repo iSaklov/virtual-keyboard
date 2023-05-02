15702447a30e93cac03dc37f818e27fa1e9c8c0e
iSaklov <ivansaklov@gmail.com>
2023-05-02 19:20:04 +0200

    fix: fixes incorrect behavior of the caps lock key

--- main.js
+++ main.js
@@ -1,301 +1,267 @@
-const os = "mac"; // Указываем операционную систему
+import keyboardLayouts from './keyboard-layout.js'
+
+const os = 'mac' // Указываем операционную систему
 
 // alert('Здесь кипит работа 🤯')
 // alert('Работа над заданием привела к глубокой депрессии')
 // alert('Буду очень признателен, если сможешь проверить меня в последнюю очередь')
 
-const languages = {
-  en: [
-    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='],
-    ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
-    ['Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
-    ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift'],
-    ['Ctrl', 'Fn', 'Option', 'Command', 'Space', 'Command', 'Option', '←', '↓', '→']
-  ],
-  ru: [
-    ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='],
-    ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ'],
-    ['Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'],
-    ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '/', 'Shift'],
-    ['Ctrl', 'Fn', 'Option', 'Command', 'Space', 'Command', 'Option', '←', '↓', '→']
-  ]
-};
+let currentLanguage = localStorage.getItem('language') || 'en' // Получаем текущий язык или устанавливаем по умолчанию английский
 
-let currentLanguage = localStorage.getItem("language") || "en"; // Получаем текущий язык или устанавливаем по умолчанию английский
+const keyboard = document.createElement('div')
+keyboard.classList.add('keyboard')
 
-const keyboardContainer = document.createElement("div");
-keyboardContainer.classList.add("keyboard");
+const textarea = document.createElement('textarea')
+textarea.classList.add('textarea')
 
-const textarea = document.createElement("textarea");
-textarea.classList.add("textarea");
+const title = document.createElement('h1')
+title.innerText =
+	currentLanguage === 'en'
+		? 'RSS Virtual Keyboard'
+		: 'RSS Виртуальная клавиатура'
+title.classList.add('title')
 
-const title = document.createElement("h1");
-title.innerText = currentLanguage === "en" ? "RSS Virtual Keyboard" : "RSS Виртуальная клавиатура";
-title.classList.add("title");
+const span1 = document.createElement('span')
+span1.innerText =
+	currentLanguage === 'en'
+		? 'The keyboard was created in the operating system Mac OS'
+		: 'Клавиатура создана в операционной системе Mac OS'
+span1.classList.add('span')
 
-const span1 = document.createElement("span");
-span1.innerText = currentLanguage === "en" ? "The keyboard was created in the operating system Mac OS" : "Клавиатура создана в операционной системе Mac OS";
-span1.classList.add("span");
+const span2 = document.createElement('span')
+span2.innerText =
+	currentLanguage === 'en'
+		? 'To switch the language, the combination: left ctrl + space'
+		: 'Для переключения языка комбинация: левый ctrl + пробел'
+span2.classList.add('span')
 
-const span2 = document.createElement("span");
-span2.innerText = currentLanguage === "en" ? "To switch the language, the combination: left ctrl + space" : "Для переключения языка комбинация: левый ctrl + пробел";
-span2.classList.add("span");
-
-document.body.append(title, textarea, keyboardContainer, span1, span2);
+document.body.append(title, textarea, keyboard, span1, span2)
 
 // Функция для добавления кнопки на клавиатуру
 function createButton(keyValue, buttonClass) {
-  const button = document.createElement("button");
-  button.classList.add("keyboard-button", buttonClass);
-  button.textContent = keyValue;
+	const button = document.createElement('button')
+	button.classList.add('keyboard-button', buttonClass)
+	button.textContent = keyValue
 
-  // Обработчик событий на клик по кнопке
-  button.addEventListener("click", () => {
-    if (keyValue === "space") {
-      textarea.value += " ";
-    } else if (keyValue === "tab") {
-      textarea.value += "  ";
-    } else if (keyValue === "enter") {
-      textarea.value += "\n";
-    } else if (keyValue === "backspace") {
-      textarea.value = textarea.value.slice(0, -1);
-    } else if (keyValue === "del") {
-      textarea.value = textarea.value.slice(0, textarea.selectionStart) + textarea.value.slice(textarea.selectionEnd);
-    } else if (keyValue === "capslock") {
-      const capsLock = document.querySelector(".capslock");
-      capsLock.classList.toggle("active");
-      toggleCapsLock();
-    } else {
-      textarea.value += keyValue[currentLanguage === "en" ? 0 : 1];
-    }
-  });
+	// Обработчик событий на клик по кнопке
+	button.addEventListener('click', () => {
+		if (keyValue === 'space') {
+			textarea.value += ' '
+		} else if (keyValue === 'tab') {
+			textarea.value += '  '
+		} else if (keyValue === 'enter') {
+			textarea.value += '\n'
+		} else if (keyValue === 'backspace') {
+			textarea.value = textarea.value.slice(0, -1)
+		} else if (keyValue === 'del') {
+			textarea.value =
+				textarea.value.slice(0, textarea.selectionStart) +
+				textarea.value.slice(textarea.selectionEnd)
+		} else if (keyValue === 'capslock') {
+			const capsLock = document.querySelector('.capslock')
+			capsLock.classList.toggle('active')
+			toggleCapsLock()
+		} else {
+			textarea.value += keyValue[currentLanguage === 'en' ? 0 : 1]
+		}
+	})
 
-  return button;
+	return button
 }
 
 // Функция для создания всей клавиатуры на основе языков
-function createKeyboard(language) {
-  const keys = languages[language].map((row) => {
-    const rowElement = document.createElement("div");
-    rowElement.classList.add("keyboard-row");
+function initKeyboard(language) {
+	const keys = keyboardLayouts[language].map((row) => {
+		const rowElement = document.createElement('div')
+		rowElement.classList.add('keyboard-row')
 
-    row.forEach((keyValue) => {
-      if (keyValue === "") {
-        rowElement.append(createSpacer());
-      } else {
-        const isLetter = /^[a-zA-Zа-яА-ЯёЁ]$/.test(keyValue);
-        const button = document.createElement("button");
-				button.classList.add("keyboard-key");
-				button.textContent = keyValue;
+		row.forEach((item) => {
+			// const isLetter = /^[a-zA-Zа-яА-ЯёЁ]$/.test(keyValue)
+			const button = document.createElement('button')
+			button.classList.add('keyboard-key')
 
-    if (isLetter) {
-      button.dataset.en = keyValue.toLowerCase();
-      button.dataset.language = language;
-    }
+			if (item.action) {
+				button.classList.add('action')
+			}
 
-    rowElement.append(button);
-  }
-});
+			button.setAttribute('data-code', item.code)
+			button.textContent = item.key
+			rowElement.append(button)
+		})
 
-return rowElement;
-});
+		return rowElement
+	})
 
-keyboardContainer.append(...keys);
-}
-
-// Функция для создания пустого промежутка между кнопками
-function createSpacer() {
-const spacer = document.createElement("div");
-spacer.classList.add("keyboard-spacer");
-return spacer;
+	keyboard.append(...keys)
 }
 
 // Создаем клавиатуру при загрузке страницы
-createKeyboard(currentLanguage);
+initKeyboard(currentLanguage)
 
 // Обработчик нажатия на клавиатуре мышью
-keyboardContainer.addEventListener("click", (event) => {
-if (event.target.classList.contains("keyboard-key")) {
-const key = event.target.textContent;
-insertText(key);
-}
-});
+keyboard.addEventListener('click', (event) => {
+	if (event.target.classList.contains('keyboard-key')) {
+		const key = event.target.textContent
+		insertText(key) //TODO
+	}
+})
 
 // Обработчик нажатия клавиш на физической клавиатуре
-document.addEventListener("keydown", (event) => {
-const key = event.key;
+document.addEventListener('keydown', (event) => {
+	const key = event.key
+	console.log('key', key)
+	console.log('event', event.code)
 
-// Отменяем стандартное поведение кнопок
-event.preventDefault();
+	// Отменяем стандартное поведение кнопок
+	event.preventDefault()
 
-// Подсвечиваем нажатую клавишу на виртуальной клавиатуре
-highlightKey(key);
+	// Подсвечиваем нажатую клавишу на виртуальной клавиатуре
+	highlightKey(event.code)
 
-// Ввод символа в текстовое поле
-if (key.length === 1 && !event.ctrlKey && !event.altKey && !event.metaKey) {
-insertText(key);
-}
+	// Ввод символа в текстовое поле
+	if (key.length === 1 && !event.ctrlKey && !event.altKey && !event.metaKey) {
+		insertText(key)
+	}
 
-// Навигация по текстовому полю
-switch (key) {
-case "ArrowUp":
-insertText("↑");
-break;
-case "ArrowDown":
-insertText("↓");
-break;
-case "ArrowLeft":
-insertText("←");
-break;
-case "ArrowRight":
-insertText("→");
-break;
-case "Enter":
-insertText("\n");
-break;
-case "Tab":
-insertText(" ");
-break;
-case "Backspace":
-deleteText(-1);
-break;
-case "Delete":
-deleteText(1);
-break;
-}
-});
+	// Навигация по текстовому полю
+	switch (key) {
+		case 'ArrowUp':
+			insertText('↑')
+			break
+		case 'ArrowDown':
+			insertText('↓')
+			break
+		case 'ArrowLeft':
+			insertText('←')
+			break
+		case 'ArrowRight':
+			insertText('→')
+			break
+		case 'Enter':
+			insertText('\n')
+			break
+		case 'Tab':
+			insertText('    ')
+			break
+		case 'Backspace':
+			deleteText(-1)
+			break
+		case 'Delete':
+			deleteText(1)
+			break
+		case 'CapsLock':
+			capslockButton.classList.toggle(
+				'active',
+				event.getModifierState('CapsLock')
+			)
+			toggleCapsLock()
+	}
+})
+
+const capslockButton = document.querySelector('button[data-code="CapsLock"]')
+capslockButton.classList.add('capslock')
+
+capslockButton.addEventListener('click', () => {
+	capslockButton.classList.toggle('active')
+	toggleCapsLock()
+})
 
 // Обработчик отжатия клавиш на физической клавиатуре
-document.addEventListener("keyup", (event) => {
-const key = event.key;
+document.addEventListener('keyup', (event) => {
+	if (event.code === 'CapsLock') {
+		capslockButton.classList.toggle(
+			'active',
+			event.getModifierState('CapsLock')
+		)
+		toggleCapsLock()
+	}
 
-// Снимаем подсветку с клавиши на виртуальной клавиатуре
-unhighlightKey(key);
-});
+	// Снимаем подсветку с клавиши на виртуальной клавиатуре
+	unhighlightKey(event.code)
+})
 
 // Функция для подсветки клавиши на виртуальной клавиатуре
-function highlightKey(key) {
-const button = getButtonByKey(key);
-if (button) {
-button.classList.add("keyboard-key-active");
-}
+function highlightKey(code) {
+	const button = getButtonByCode(code)
+	if (button) {
+		button.classList.add('active')
+	}
 }
 
 // Функция для снятия подсветки с клавиши на виртуальной клавиатуре
-function unhighlightKey(key) {
-const button = getButtonByKey(key);
-if (button) {
-button.classList.remove("keyboard-key-active");
-}
+function unhighlightKey(code) {
+	const button = getButtonByCode(code)
+	if (button) {
+		button.classList.remove('active')
+	}
 }
 
-// Функция для получения кнопки на виртуальной клавиатуре по символу
-function getButtonByKey(key) {
-return keyboardContainer.querySelector(`button:not([data-en])[data-key="${key}"]`);
+// Функция для получения кнопки на виртуальной клавиатуре по коду
+function getButtonByCode(code) {
+	return keyboard.querySelector(`button[data-code="${code}"]`)
 }
 
 // Функция для вставки символа в текстовое поле
-function insertText(text) {
-const textarea = document.querySelector(".textarea");
-const cursorPosStart = textarea.selectionStart;
-const cursorPosEnd = textarea.selectionEnd;
-const currentValue = textarea.value;
-textarea.value =
-currentValue.substring(0, cursorPosStart) +
-text +
-currentValue.substring(cursorPosEnd);
-textarea.setSelectionRange(cursorPosStart + 1, cursorPosStart + 1);
-textarea.focus();
+function insertText(string) {
+	const textarea = document.querySelector('.textarea')
+	const cursorPosStart = textarea.selectionStart
+	const cursorPosEnd = textarea.selectionEnd
+	const currentValue = textarea.value
+
+	if (capslockButton.classList.contains('active')) {
+		string = string.toUpperCase()
+	}
+
+	textarea.value =
+		currentValue.substring(0, cursorPosStart) +
+		string +
+		currentValue.substring(cursorPosEnd)
+	textarea.setSelectionRange(
+		cursorPosStart + string.length,
+		cursorPosStart + string.length
+	)
+	textarea.focus()
 }
 
 // Функция для удаления символа в текстовом поле
 function deleteText(direction) {
-  const startPosition = textarea.selectionStart;
-  const endPosition = textarea.selectionEnd;
-  textarea.value = textarea.value.slice(0, startPosition) + textarea.value.slice(endPosition);
-  textarea.selectionEnd = textarea.selectionStart = startPosition + direction;
-}
+	const textarea = document.querySelector('.textarea')
+	const startPosition = textarea.selectionStart
+	const endPosition = textarea.selectionEnd
+	// console.log(startPosition, endPosition)
 
-// Функция для вставки символа-разделителя
-function insertSeparator(separator) {
-const textarea = document.querySelector(".textarea");
-const cursorPosStart = textarea.selectionStart;
-const cursorPosEnd = textarea.selectionEnd;
-const currentValue = textarea.value;
-textarea.value =
-currentValue.substring(0, cursorPosStart) +
-separator +
-currentValue.substring(cursorPosEnd);
-textarea.setSelectionRange(cursorPosStart + separator.length, cursorPosStart + separator.length);
-textarea.focus();
+	if (startPosition === endPosition && startPosition > 0) {
+		textarea.value =
+			textarea.value.slice(0, startPosition - 1) +
+			textarea.value.slice(endPosition)
+		textarea.selectionEnd = textarea.selectionStart = startPosition - 1
+	} else {
+		textarea.value =
+			textarea.value.slice(0, startPosition) + textarea.value.slice(endPosition)
+		textarea.selectionEnd = textarea.selectionStart = startPosition + direction
+	}
 }
 
 // Функция для переключения режима CapsLock
 function toggleCapsLock() {
-  const buttons = document.querySelectorAll(".keyboard-button");
-  buttons.forEach((button) => {
-    if (/^[a-zA-Zа-яА-ЯёЁ]$/.test(button.textContent)) {
-      button.textContent = button.textContent.toUpperCase();
-    }
-  });
+	const buttons = document.querySelectorAll('.keyboard-key')
+	buttons.forEach((button) => {
+		if (/^[a-zA-Zа-яА-ЯёЁ]$/.test(button.textContent)) {
+			if (capslockButton.classList.contains('active')) {
+				button.textContent = button.textContent.toUpperCase()
+			} else {
+				button.textContent = button.textContent.toLowerCase()
+			}
+		}
+	})
 }
-
-// Обработчик событий нажатия на кнопки клавиатуры
-document.addEventListener("keydown", (event) => {
-const key = document.querySelector(`[data-code="${event.code}"]`);
-if (key) {
-key.classList.add("active");
-if (key.classList.contains("letter")) {
-const language = localStorage.getItem("language") || "en";
-const keyValue = event.shiftKey ? key.dataset.shift : key.dataset.value;
-const upperKeyValue = keyValue.toUpperCase();
-insertText(event.getModifierState("CapsLock") ? upperKeyValue : keyValue);
-} else if (key.classList.contains("number")) {
-const keyValue = event.shiftKey ? key.dataset.shift : key.dataset.value;
-insertText(keyValue);
-} else if (key.classList.contains("separator")) {
-const keyValue = key.dataset.value;
-insertSeparator(keyValue);
-} else if (key.classList.contains("space")) {
-insertText(" ");
-} else if (key.classList.contains("backspace")) {
-const textarea = document.querySelector(".textarea");
-const cursorPosStart = textarea.selectionStart;
-const cursorPosEnd = textarea.selectionEnd;
-if (cursorPosStart === cursorPosEnd) {
-textarea.setSelectionRange(cursorPosStart - 1, cursorPosStart - 1);
-}
-document.execCommand("delete");
-} else if (key.classList.contains("delete")) {
-const textarea = document.querySelector(".textarea");
-const cursorPosStart = textarea.selectionStart;
-const cursorPosEnd = textarea.selectionEnd;
-if (cursorPosStart === cursorPosEnd) {
-textarea.setSelectionRange(cursorPosStart + 1, cursorPosStart + 1);
-}
-document.execCommand("delete");
-} else if (key.classList.contains("tab")) {
-insertSeparator(" ");
-} else if (key.classList.contains("enter")) {
-insertSeparator("\n");
-}
-event.preventDefault();
-}
-});
-
-document.addEventListener("keyup", (event) => {
-const key = document.querySelector(`[data-code="${event.code}"]`);
-if (key) {
-key.classList.remove("active");
-}
-});
 
 // Функция для переключения языка клавиатуры
 function switchLanguage() {
-const language = localStorage.getItem("language") || "en";
-const newLanguage = language === "en" ? "ru" : "en";
-localStorage.setItem("language", newLanguage);
-createKeyboard(newLanguage);
+	const language = localStorage.getItem('language') || 'en'
+	const newLanguage = language === 'en' ? 'ru' : 'en'
+	localStorage.setItem('language', newLanguage)
+	initKeyboard(newLanguage)
 }
 
 // Обработчик события переключения языка клавиатуры
@@ -311,16 +277,16 @@ createKeyboard(newLanguage);
 // });
 
 // Функция для обновления клавиатуры на новом языке
-function initKeyboard() {
-const keyboardRows = document.querySelectorAll(".keyboard-row");
-keyboardRows.forEach((row, i) => {
-const keys = row.querySelectorAll(".key");
-keys.forEach((key, j) => {
-const keyValue = languages[currentLanguage][i].split(" ")[j];
-key.textContent = keyValue;
-});
-});
-}
+// function initKeyboard() {
+// 	const keyboardRows = document.querySelectorAll('.keyboard-row')
+// 	keyboardRows.forEach((row, i) => {
+// 		const keys = row.querySelectorAll('.key')
+// 		keys.forEach((key, j) => {
+// 			const keyValue = languages[currentLanguage][i].split(' ')[j]
+// 			key.textContent = keyValue
+// 		})
+// 	})
+// }
 
 // Инициализация клавиатуры при загрузке страницы
 // initKeyboard();
