a0b43726944936b95e16434de7dd92dca1953dc6
iSaklov <ivansaklov@gmail.com>
2023-05-04 12:35:51 +0200

    feat: add eslint-config-airbnb-base

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,77 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const postcssPresetEnv = require('postcss-preset-env');
 
-const mode = process.env.NODE_ENV || 'development'
-const devMode = mode === 'development'
-const target = devMode ? 'web' : 'browserslist'
-const devtool = devMode ? 'source-map' : undefined
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
 
 module.exports = {
-	mode,
-	target,
-	devtool,
-	devServer: {
-		port: 3000,
-		open: true,
-		hot: true,
-	},
-	entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'main.js')],
-	output: {
-		filename: '[name].[contenthash].js',
-		path: path.resolve(__dirname, 'dist'),
-		clean: true,
-		assetModuleFilename: 'assets/[name][ext]'
-	},
-	optimization: {
+  mode,
+  target,
+  devtool,
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'main.js')],
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: 'assets/[name][ext]',
+  },
+  optimization: {
     runtimeChunk: 'single',
   },
-	plugins: [
-		new HtmlWebpackPlugin({
-			filename: '[name].[contenthash].html',
-			template: path.resolve(__dirname, 'src', 'index.html')
-		}),
-		new MiniCssExtractPlugin({
-			filename: '[name].[contenthash].css'
-		})
-	],
-	module: {
-		rules: [
-			{
-				test: /\.html$/i,
-				loader: 'html-loader',
-			},
-			{
-				test: /\.(c|sa|sc)ss$/i,
-				use: [
-					devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-					'css-loader',
-					{
-						loader: 'postcss-loader',
-						options: {
-							postcssOptions: {
-								plugins: [require('postcss-preset-env')],
-							},
-						},
-					},
-					'sass-loader'
-				]
-			},
-			{
-				test: /\.(?:ico|gif|svg|png|jpg|jpeg)$/i,
-				type: 'asset/resource'
-			},
-			{
-      test: /\.(?:js|mjs|cjs)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            ['@babel/preset-env', { targets: "defaults" }]
-          ]
-        }
-      }
-    }
-		]
-	}
-}
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: '[name].[contenthash].html',
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [postcssPresetEnv],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(?:ico|gif|svg|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+    ],
+  },
+};
